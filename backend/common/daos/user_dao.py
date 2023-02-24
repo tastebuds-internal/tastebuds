202245bfe97516b07740dd2f59089b865222f79a
Amar Ramachandran <amar@neeva.co>
2023-02-24 06:46:47 +0000

    Add UserDao

---
+++ backend/common/daos/user_dao.py
@@ -0,0 +1,21 @@
+import uuid
+from .base import db
+from sqlalchemy import Text
+from sqlalchemy.dialects.postgresql import UUID
+
+class UserDao(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(UUID, default=uuid.uuid4, nullable=False, primary_key=True)
+    first_name = db.Column(Text, nullable=False)
+    last_name = db.Column(Text, nullable=False)
+
+    @classmethod
+    def get_by_id(cls, _id):
+        return cls.query.get(_id)
+
+    def __eq__(self, other):
+        return type(self) is type(other) and self.id == other.id
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
---
+++ backend/devdb.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, first_name, last_name) VALUES ('00000000-0000-0000-0000-000000000000', 'SpongeBob', 'SquarePants');
+INSERT INTO users (id, first_name, last_name) VALUES ('11111111-1111-1111-1111-111111111111', 'Patrick', 'Star');
+INSERT INTO users (id, first_name, last_name) VALUES ('22222222-2222-2222-2222-222222222222', 'Mr.', 'Krabs');
+INSERT INTO users (id, first_name, last_name) VALUES ('33333333-3333-3333-3333-333333333333', 'Squidward', 'Tentacles');
+COMMIT;

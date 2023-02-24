40184d474e4d2c663a10f87e7c01d7f79878e7e9
Zubin Aysola <zubinaysola@gmail.com>
2023-02-23 22:13:25 -0800

    foodfinder

---
+++ learning/model/sentiment.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import Iterable
+from typing import Iterable
+import transformers
+import torch
+from pytorch_lightning import LightningModule
+
+
+class Model(ABC):
+    @abstractmethod
+    def infer(self, input: Iterable, **kwargs):
+        pass
+
+    def __call__(self, **kwargs):
+        return self.infer(**kwargs)
+
+
+class FoodFinder(Model):
+    def __init__(self):
+        self.trained = True

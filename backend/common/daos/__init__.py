91fb4a6ab3d9a423c3ea0d37c32a361d65174048
Amar Ramachandran <amar@neeva.co>
2023-02-24 06:55:11 +0000

    Add RestaurantDao

--- backend/common/daos/__init__.py
+++ backend/common/daos/__init__.py
@@ -1,1 +1,2 @@
 from .user_dao import UserDao
+from .restaurant_dao import RestaurantDao
---
+++ backend/common/daos/restaurant_dao.py
@@ -0,0 +1,20 @@
+import uuid
+from .base import db
+from sqlalchemy import Text
+from sqlalchemy.dialects.postgresql import UUID
+
+class RestaurantDao(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(UUID, default=uuid.uuid4, nullable=False, primary_key=True)
+    name = db.Column(Text, nullable=False)
+
+    @classmethod
+    def get_by_id(cls, _id):
+        return cls.query.get(_id)
+
+    def __eq__(self, other):
+        return type(self) is type(other) and self.id == other.id
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
